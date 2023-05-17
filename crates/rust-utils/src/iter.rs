b8a54b74614d8bfd2ab3d4fe098022e4f94bc147
Nugine <nugine@foxmail.com>
2023-05-17 12:39:12 +0800

    add `filter_map_collect`

--- crates/rust-utils/src/iter.rs
+++ crates/rust-utils/src/iter.rs
@@ -17,5 +17,24 @@ where
     I: IntoIterator,
     F: FnMut(I::Item) -> T,
 {
-    iterable.into_iter().map(f).collect()
+    map_collect(iterable, f)
+}
+
+pub fn filter_map_collect<C, T, I, F>(iterable: I, f: F) -> C
+where
+    I: IntoIterator,
+    F: FnMut(I::Item) -> Option<T>,
+    C: FromIterator<T>,
+{
+    iterable.into_iter().filter_map(f).collect()
+}
+
+#[cfg_attr(docsrs, doc(cfg(feature = "alloc")))]
+#[cfg(feature = "alloc")]
+pub fn filter_map_collect_vec<T, I, F>(iterable: I, f: F) -> Vec<T>
+where
+    I: IntoIterator,
+    F: FnMut(I::Item) -> Option<T>,
+{
+    filter_map_collect(iterable, f)
 }
